b509b7e89cd030e03212ce8f14805c094a105bff
APareja12 <alexis.pareja@hotmail.com>
2023-02-28 15:08:04 -0500

    creates createBoard function

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const gamesBoardContainer = document.querySelector('#gamesboard-container')
 const optionContainer = document.querySelector('.option-container')
 const flipButton = document.querySelector('#flip-button')
 
@@ -12,5 +13,19 @@ const optionShips = Array.from(optionContainer.children)
    angle = angle === 0 ? 90 : 0
     optionShips.forEach(optionShip => optionShip.style.transform = `rotate(${angle}deg)`)
 }
+
+// Creating Boards 
+const width = 10
+
+function createBoard(color) {
+    const gameBoardContainer = document.createElement('div')
+    gameBoardContainer.classList.add('game-board')
+    gameBoardContainer.style.backgroundColor = color
+
+    gamesBoardContainer.append(gameBoardContainer)
+}
+
+createBoard('yellow')
+createBoard('pink')
 
 flipButton.addEventListener('click', flip)
