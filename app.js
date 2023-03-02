d22e8dfef309f45a9740ef9560b0cf352fd0ed86
APareja12 <alexis.pareja@hotmail.com>
2023-03-02 10:40:41 -0500

    changes startIndex

--- app.js
+++ app.js
@@ -54,15 +54,19 @@ const carrier = new Ship('carrier', 5)
 
 const ships = [destroyer,submarine, cruiser, battleship, carrier]
 
-function addShipPiece(ship) {
-    const allBoardBlocks = document.querySelectorAll('#computer div')
+let notDropped
+
+function addShipPiece(user, ship, startId) {
+    const allBoardBlocks = document.querySelectorAll(`#${user} div`)
     let randomBoolean = Math.random() < 0.5
-    let isHorizontal = randomBoolean
+    let isHorizontal = user === 'player' ? angle === 0 : randomBoolean
     let randomStartIndex = Math.floor(Math.random() * width * width)
 
-    let validStart = isHorizontal ? randomStartIndex <= width * width - ship.length ? randomStartIndex : width * width - ship.length :
+    let startIndex = startId ? startId : randomStartIndex
+
+    let validStart = isHorizontal ? startIndex <= width * width - ship.length ? startIndex : width * width - ship.length :
     //handle vertical
-    randomStartIndex <= width * width - width * ship.length ? randomStartIndex : randomStartIndex - ship.length * width + width
+    startIndex <= width * width - width * ship.length ? startIndex : startIndex - ship.length * width + width
 
     let shipBlocks = []
 
@@ -93,8 +97,41 @@ function addShipPiece(ship) {
             shipBlock.classList.add('taken')
         })
     } else {
-        addShipPiece(ship)
+        if (user === 'computer') addShipPiece(ship)
+        if (user === 'player') notDropped = true
     }
 }  
 
-ships.forEach(ship => addShipPiece(ship))
+ships.forEach(ship => addShipPiece('computer', ship))
+
+// Drag player ships
+let draggedShip 
+const optionShips = Array.from(optionContainer.children)
+optionShips.forEach(optionShip => optionShip.addEventListener('dragstart', dragStart))
+
+const allPlayerBlocks = document.querySelectorAll('#player div')
+allPlayerBlocks.forEach(playerBlock => {
+    playerBlock.addEventListener('dragover', dragOver)
+    playerBlock.addEventListener('drop', dropShip)
+})
+
+function dragStart(e) {
+    notDropped = false
+    draggedShip = e.target 
+}
+
+function dragOver(e) {
+    e.preventDefault()
+}
+
+function dropShip(e) {
+    const startId = e.target.id
+    const ship = ships[draggedShip.id]
+    addShipPiece('player', ship, startId)
+    if (!notDropped) {
+        draggedShip.remove()
+    }
+}
+
+// Add highlight
+function highlightArea( startIndex, ship)
